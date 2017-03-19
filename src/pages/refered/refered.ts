8d76d82bde38c4a426dfe01e303af96ed7a89498
paharo <potopelao2@gmail.com>
2017-03-18 20:31:20 -0400

    Agregado al menu el nombre de menu

--- src/pages/refered/refered.ts
+++ src/pages/refered/refered.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/*
-  Generated class for the Refered page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-refered',
   templateUrl: 'refered.html'
 })
 export class ReferedPage {
-
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
 
