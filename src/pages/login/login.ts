6fee22d138037eff9cc48ad6533b4acfb5dc384a
Oswald <oswald.capriles46@gmail.com>
2017-04-29 16:17:48 -0400

    Agregado prompt para borrar el token de google, y agregado un parametro extra al request la obtener el token

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,42 +1,83 @@
 import { Component } from '@angular/core';
 import { GooglePlus } from '@ionic-native/google-plus';
 
-import { MenuController,NavController } from 'ionic-angular';
+import { 
+	MenuController,
+	NavController,
+	AlertController
+} from 'ionic-angular';
+
 import { ReferedPage } from '../refered/refered';
 import { GuideLoginPage } from '../guide-login/guide-login';
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
+	selector: 'page-login',
+	templateUrl: 'login.html'
 })
 export class Login {
-  constructor(
+	constructor(
+		private alertCtrl: AlertController,
 		private googlePlus: GooglePlus,
 		private menu: MenuController, 
 		private navCtrl : NavController) {}
 
-  ionViewDidEnter() {
-  	// Cerrar sesion cuando entra para estar seguros.
-  	this.menu.enable(false);
-  }
-	
+	ionViewDidEnter() {
+		// Cerrar sesion cuando entra para estar seguros.
+		this.menu.enable(false);
+	}
+
 	// Luego pensamos donde vamos a poner esto
-  refered() {
-  	this.navCtrl.push(ReferedPage);
-  }
-	
+	refered() {
+		this.navCtrl.push(ReferedPage);
+	}
+
 	// Google Oauth Login
 	// Todavia falta mucho por descubrir
 	googleLogin() {
-		
-		console.log('Google Login!');
-		this.googlePlus.login({})
-				.then(res => console.log(res))
-				.catch(err => console.log(err));
 
+		console.log('Google Login!');
+		this.googlePlus.login({
+			// Sera seguro dejar esto aqui ? 
+			'webClientId': '96779452065-5jeqgrfmlp64eptahte2eq1hatl5rkfe.apps.googleusercontent.com'
+		})
+			.then(this.googleResponse.bind(this))
+			.catch(err => console.log(err));
+
+		// Mas tarde
+		// this.navCtrl.push(MainPage);
 	}
 
-  guideLogin() {
-    this.navCtrl.push(GuideLoginPage);
-  }
+	private googleResponse(res) {
+
+		console.log(res);
+		let alert = this.alertCtrl.create({
+			title: 'Login',
+			subTitle: 'Google ha respondido!',
+			buttons: [
+				{
+					text: 'Cancel',
+					role: 'cancel',
+					handler: data => {
+						console.log('Disconnect dat token!');
+						this.googlePlus.logout();
+					}
+				},
+				{
+					text: 'Keep',
+					handler: data => {
+						// No hace algo...
+						console.log('Keep dat token!');
+					}
+				}
+			]
+		});
+		alert.present();
+
+		// Aqui debe mandar al servidor 
+		// el token de Google
+	}
+
+	guideLogin() {
+		this.navCtrl.push(GuideLoginPage);
+	}
 }
