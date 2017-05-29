9d8114618729b0be49449424ff02e11d9768191f
Oswald <oswald.capriles46@gmail.com>
2017-05-29 00:36:36 -0400

    Agregado querys para creacion de la base de datos local

--- src/providers/offline-provider.ts
+++ src/providers/offline-provider.ts
@@ -12,7 +12,7 @@ import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 @Injectable()
 export class OfflineProvider {
 
-	dbConnection: Promise<SQLiteObject>;
+	dbConnect: Promise<SQLiteObject>;
 
 	constructor(private sqlite: SQLite) {
 
@@ -25,23 +25,114 @@ export class OfflineProvider {
 
 	public initDatabase() {
 
-		this.dbConnection = this.sqlite.create({
+		let dbc = this.sqlite.create({
 			name: 'data.db',
 			location: 'default'
-		})
-			.then((db: SQLiteObject) => db)
-			.catch((e) => console.log(e));
+		});
 
+		this.dbConnect = dbc;
+		dbc.then(this.initUserTable.bind(this));
+		dbc.then(this.initGuideTable.bind(this))
+		dbc.then(this.initUserGuideTable.bind(this))
+		dbc.catch((e) => console.log(e));
 	}
 
-	storeUserInfo() {
+	initUserTable(db: SQLiteObject) {
 
-		this.dbConnection
-			.then((db: SQLiteObject) => {
-				// db.executeSql('');
-				console.log("Ejecutado el sql!");
-			})
-			.catch((e) => console.log(e));
+		let sql = "";
+		sql += 'CREATE TABLE IF NOT EXISTS USERS(';
+		sql += 'ID INTEGER PRIMARY KEY AUTOINCREMENT,';
+		sql += 'TYPE INTEGER,';
+		sql += 'PAGE INTEGER,';
+		sql += 'REFERER INTEGER,';
+		sql += 'NAME VARCHAR(255) NOT NULL,';
+		sql += 'EMAIL VARCHAR(255) NOT NULL,';
+		sql += 'DOLLARS DECIMAL(32, 2)';
+		sql += ');';
 
+		db.executeSql(sql, {});
+		console.log("Created Users Table!");
+
+		return db;
+	}
+
+	initGuideTable(db: SQLiteObject) {
+
+		let sqlQuerys = [];
+
+		// Un poco feo, se pueden mover 
+		// estos querys a otro archivo...
+
+		let sql = "";
+		sql += 'CREATE TABLE IF NOT EXISTS ZONES(';
+		sql += 'ID INTEGER PRIMARY KEY AUTOINCREMENT,';
+		sql += 'NAME VARCHAR(255) NOT NULL,';
+		sql += 'POLYGON VARCHAR(255) NOT NULL';
+		sql += ');';
+		sqlQuerys.push(sql);
+
+		sql = "";
+		sql += 'CREATE TABLE IF NOT EXISTS GUIDES('
+		sql += 'ID INTEGER PRIMARY KEY AUTOINCREMENT,'
+		sql += 'ZONE_ID INTEGER NOT NULL,'
+		sql += 'LANG CHAR(20),'
+		sql += 'FOREIGN KEY (ZONE_ID) REFERENCES ZONES(ID)'
+		sql += ');'
+		sqlQuerys.push(sql);
+
+		sql = "";
+		sql += 'CREATE TABLE IF NOT EXISTS SUB_ZONES(';
+		sql += 'ID INTEGER PRIMARY KEY AUTOINCREMENT,';
+		sql += 'ZONE_ID INTEGER NOT NULL,';
+		sql += 'NAME VARCHAR(255) NOT NULL,';
+		sql += 'POLYGON VARCHAR(255) NOT NULL,';
+		sql += 'FOREIGN KEY (ZONE_ID) REFERENCES ZONES(ID)';
+		sql += ');';
+		sqlQuerys.push(sql);
+
+		sql = "";
+		sql += 'CREATE TABLE IF NOT EXISTS SPOTS(';
+		sql += 'ID INTEGER PRIMARY KEY AUTOINCREMENT,';
+		sql += 'SUB_ZONE_ID INTEGER NOT NULL,';
+		sql += 'CATEGORY VARCHAR(255) NOT NULL,';
+		sql += 'NAME VARCHAR(255) NOT NULL,';
+		sql += 'DESCRIPTION TEXT,';
+		sql += 'POINT VARCHAR(255) NOT NULL,';
+		sql += 'FOREIGN KEY (SUB_ZONE_ID) REFERENCES SUB_ZONES(ID)';
+		sql += ');';
+		sqlQuerys.push(sql);
+
+		sql = "";
+		sql += 'CREATE TABLE IF NOT EXISTS AUDIOS(';
+		sql += 'ID INTEGER PRIMARY KEY AUTOINCREMENT,';
+		sql += 'SPOT_ID INTEGER NOT NULL,';
+		sql += 'LANG CHAR(20),';
+		sql += 'PATH VARCHAR(255) NOT NULL,';
+		sql += 'FOREIGN KEY (SPOT_ID) REFERENCES SPOTS(ID)';
+		sql += ');';
+
+		console.log(sqlQuerys);
+
+		db.sqlBatch(sqlQuerys);
+		console.log("Created Guide Tables!");
+
+		return db;
+	}
+
+	initUserGuideTable(db: SQLiteObject) {
+
+		let sql = "";
+		sql += 'CREATE TABLE IF NOT EXISTS USER_GUIDE(';
+		sql += 'USER_ID INTEGER NOT NULL,';
+		sql += 'GUIDE_ID INTEGER NOT NULL,';
+		sql += 'FOREIGN KEY (USER_ID) REFERENCES USERS(ID),';
+		sql += 'FOREIGN KEY (GUIDE_ID) REFERENCES GUIDES(ID),';
+		sql += 'PRIMARY KEY (USER_ID, GUIDE_ID)';
+		sql += ');';
+
+		db.executeSql(sql, {});
+		console.log("Created User relationship with Guide");
+
+		return db;
 	}
 }
